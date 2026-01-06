6940db8b9689d9d2ddff0a52947842f9a34a8d5c
Rqq-al22 <rezkialya0909@gmail.com>
2026-01-06 15:56:12 +0800

    Update smtp_config.php
    Signed-off-by: Rqq-al22 <rezkialya0909@gmail.com>

--- Setara3/setara 2/api/smtp_config.php
+++ Setara3/setara 2/api/smtp_config.php
@@ -3,11 +3,11 @@
 // Silakan isi detail akun email pengirim di sini
 
 return [
-    'smtp_host' => 'smtp.gmail.com',      // Host SMTP (contoh: smtp.gmail.com)
-    'smtp_port' => 587,                   // Port SMTP (587 untuk TLS, 465 untuk SSL)
-    'smtp_user' => 'your_email@gmail.com', // Email pengirim
-    'smtp_pass' => 'your_app_password',    // Password aplikasi (App Password jika pakai 2FA Gmail)
-    'from_email' => 'no-reply@setarakids.com', // Email yang muncul di 'From'
-    'from_name' => 'Setara Kids System'    // Nama yang muncul di 'From'
+    'smtp_host' => 'mail.setarakidsschool.sch.id',      // Host SMTP (contoh: smtp.gmail.com)
+    'smtp_port' => 465,                   // Port SMTP (587 untuk TLS, 465 untuk SSL)
+    'smtp_user' => 'no-reply@setarakidsschool.com', // Email pengirim
+    'smtp_pass' => 'setara0099_',    // Password aplikasi (App Password jika pakai 2FA Gmail)
+    'from_email' => 'no-reply@setarakidsschool.com', // Email yang muncul di 'From'
+    'from_name' => 'Setara Kids School'    // Nama yang muncul di 'From'
 ];
 ?>
